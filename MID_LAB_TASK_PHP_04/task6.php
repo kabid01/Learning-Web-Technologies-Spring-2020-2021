2c6261a2ec7eaa214e043b3d89591bbc3d5ae23e
kabid01 <78007491+kabid01@users.noreply.github.com>
2021-02-16 11:34:42 +0600

    Add files via upload

---
+++ MID_LAB_TASK_PHP_04/task6.php
@@ -0,0 +1,17 @@
+
+<?php
+
+    $arr= ['kabid','18-38144-2','Webtech'];
+    
+    $SearchElement=kabid;
+
+    foreach($arr as $val)
+    {
+        if($val==$SearchElement)
+        
+        {
+            echo"$SearchElement is present in the array.\n";
+        }
+    }
+
+?>
