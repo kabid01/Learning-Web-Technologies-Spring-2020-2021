7c3e518c4ab387bd53167bc11874c3a7d6b8f80a
kabid01 <78007491+kabid01@users.noreply.github.com>
2021-02-16 11:22:43 +0600

    Add files via upload

---
+++ MID_LAB_TASK_PHP_04/task1.php
@@ -0,0 +1,11 @@
+<?php 
+    $length=15;
+    $width=8;
+    
+     $area =    ($length*$width);
+    $perimeter = 2*($length+$width);
+
+    echo("Area of Rectangle: $area \n");
+    
+    echo("Perimeter of Rectangle : $perimeter \n");
+?>
